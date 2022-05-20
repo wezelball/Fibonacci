3359ecfeee2142bf5e578f5c3d2263fe88e70ee7
David Cohen <dcohen@pearsonpkg.com>
2022-05-20 06:22:18 -0400

    My algorithm is faulty

--- Main.java
+++ Main.java
@@ -10,10 +10,12 @@ public class Main {
             while(jackpot == false) {
                 xn = fib.getFibonacci(i);
                 String numberString = Integer.toString(xn);
+                
+                System.out.println("Fibonacci number: " + xn);
                 System.out.println("Number of digits so far: " + numberString.length());
                 
                 // Now for the juice - when the length of xn os 1000 digits, print the answer
-                if (numberString.length() == 1000) {
+                if (numberString.length() == 12) {
                     System.out.println("Jackpot! You have reached the 1000-digit term!");
                     System.out.println("The fibonacci value of term " + i + " is " + numberString);
                     jackpot = true;
